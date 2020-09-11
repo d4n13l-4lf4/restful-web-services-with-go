7e371c9f62cefa3fe3bde64d412c335ac23a31f4
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-09-10 22:48:37 -0500

    httprouter Example

---
+++ chapter02/httprouterExample/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func getCommandOutput(command string, arguments ...string) string {
+	out, err := exec.Command(command, arguments...).Output()
+	if err != nil {
+		return err.Error()
+	}
+	return string(out)
+}
+
+func goVersion(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	response := getCommandOutput("/usr/lib/golang/bin/go", "version")
+	io.WriteString(w, response)
+	return
+}
+
+func getFileContent(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintf(w, getCommandOutput("/bin/cat", params.ByName("name")))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/api/v1/go-version", goVersion)
+	router.GET("/api/v1/show-file/:name", getFileContent)
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
